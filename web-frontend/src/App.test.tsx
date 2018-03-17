5727d9c91f1de21d0b863237f879e8fc3406073f
Andreas Sahle <andreas.sahle@wunder.io>
2018-03-17 17:02:27 +0100

    test setup with enzyme

--- web-frontend/src/App.test.tsx
+++ web-frontend/src/App.test.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const tree = shallow(<App />);
+  expect(toJson(tree)).toMatchSnapshot();
 });
